db8220512f4b99b40161c806e058f06e82bb8463
soohyn <sooheyonjo@gmail.com>
2026-02-07 14:16:48 +0900

    feat: kakao 책 api 함수 작성 및 관련 타입 작성

---
+++ src/api/book.ts
@@ -0,0 +1,27 @@
+const KAKAO_REQUEST_URL = "https://dapi.kakao.com/v3/search/book";
+
+type Sort = "accuracy" | "latest";
+
+type Target = "title" | "isbn" | "pulisher" | "person";
+
+interface RequestBooksParams {
+  query: string;
+  sort?: Sort;
+  page?: number;
+  size?: number;
+  target?: Target;
+}
+
+export const requestBooks = async (
+  params: RequestBooksParams,
+): Promise<ResponseData> => {
+  const urlParams = new URLSearchParams(Object.entries(params)).toString();
+  const requestUrl = `${KAKAO_REQUEST_URL}?${urlParams}`;
+  const headers = {
+    Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
+  };
+
+  const response = await fetch(requestUrl, { headers });
+
+  return response.json();
+};
---
+++ src/types/global.d.ts
@@ -0,0 +1,27 @@
+type Status = string | "정상판매";
+
+interface Meta {
+  total_count: number;
+  pageable_count: number;
+  is_end: boolean;
+}
+
+interface Document {
+  authors: string[];
+  contents: string;
+  datetime: Date;
+  isbn: string;
+  price: number;
+  publisher: string;
+  sale_price: number;
+  status: Status;
+  thumbnail: string;
+  title: string;
+  translators: string[];
+  url: string;
+}
+
+interface ResponseData {
+  meta: Meta;
+  documents: Document[];
+}
