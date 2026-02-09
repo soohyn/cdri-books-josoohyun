7584692c0d94180f0c74ce503ce9d1d615c7c2df
soohyn <sooheyonjo@gmail.com>
2026-02-09 17:40:09 +0900

    feat: like 데이터 가져오기

--- src/pages/Like.tsx
+++ src/pages/Like.tsx
@@ -1,4 +1,8 @@
-import { useQueryClient } from "@tanstack/react-query";
+import {
+  useQueryClient,
+  type InfiniteData,
+  type QueryKey,
+} from "@tanstack/react-query";
 import BookListItemContainer from "../components/book/BookListItemContainer";
 import NoData from "../components/NoData";
 import { useLikeStorage } from "../hooks/useLikeStorage";
@@ -12,13 +16,21 @@ function Like() {
   const { likes, toggleLike } = useLikeStorage();
   const queryClient = useQueryClient();
 
-  const data = queryClient.getQueriesData<BooksResponse>({
+  const data = queryClient.getQueriesData<
+    Array<[QueryKey, InfiniteData<BooksResponse>]>
+  >({
     queryKey: ["books"],
   });
 
-  const filteredBooks = data
-    .map((d) => d[1]?.documents.filter((item) => likes.includes(item.title)))
-    .flat() as Document[];
+
+ const filteredBooks = data
+  .map((d) =>
+    d[1]?.pages
+      .flatMap((page) => page.documents)
+      .filter((item) => likes.includes(item.title))
+  )
+  .filter((v): v is Document[] => Array.isArray(v))
+  .flat();
 
   const itemMap = filteredBooks.map((mapItem, index) => {
     return (
